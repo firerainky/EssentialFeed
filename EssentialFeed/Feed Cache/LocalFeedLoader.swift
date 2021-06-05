8ff019debea0d3eb4815f11659d27a9180523baf
nameky <suchbalance@qq.com>
2021-06-05 22:13:55 +0800

    Group cases to remove duplicate code.

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -39,9 +39,7 @@ public class LocalFeedLoader {
                 completion(.failure(error))
             case let .found(local, timestamp) where self.validate(timestamp):
                 completion(.success(local.toModels()))
-            case .found:
-                completion(.success([]))
-            case .empty:
+            case .found, .empty:
                 completion(.success([]))
             }
         }
