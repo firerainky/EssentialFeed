9b616cae1c948907a33e3b548fc3d98c033cf1de
nameky <suchbalance@qq.com>
2021-06-08 18:57:54 +0800

    Retrieve delivers failure on retrieval error (invalid cached data).

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -43,9 +43,13 @@ class CodableFeedStore {
             return completion(.empty)
         }
         let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
-
-        completion(.found(feed: cache.feed.map { $0.local }, timestamp: cache.timestamp))
+        
+        do {
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.feed.map { $0.local }, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
@@ -98,6 +102,12 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
     }
     
+    func test_retrieve_deliversFailureOnReceivalError() {
+        let sut = makeSUT()
+        try! "invalid data".write(to: testSpecificStoreURL(), atomically: true, encoding: .utf8)
+        expect(sut, toRetrieve: .failure(anyNSError()))
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
@@ -120,7 +130,7 @@ class CodableFeedStoreTests: XCTestCase {
         
         sut.retrieve { result in
             switch (result, expectedResult) {
-            case (.empty, .empty):
+            case (.empty, .empty), (.failure, .failure):
                 break
             case let (.found(feed: receivedFeed, timestamp: receivedTimestamp), .found(feed: expectedFeed, timestamp: expectedTimestamp)):
                 XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
