1df7ae2e4e5201aa28cf940a40c0bddd01509396
nameky <suchbalance@qq.com>
2021-06-08 16:01:10 +0800

    Move store URL location from `.documentDirectory` to `.cacheDirectory`.

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -131,7 +131,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func testSpecificStoreURL() -> URL {
-        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
     }
     
     private func setupEmptyStoreState() {
